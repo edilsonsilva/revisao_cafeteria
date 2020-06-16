c19f37237a7ae19f14710e26b996220f6041d5f5
Edilson Silva <edilson_js@yahoo.com.br>
2020-06-16 10:18:11 -0300

    Criação das demais páginas do site

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function abrir() {
   document.getElementById("sidenav").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "300px";
+  // document.getElementById("main").style.marginLeft = "300px";
 }
 function fechar() {
   document.getElementById("sidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  // document.getElementById("main").style.marginLeft = "0";
 }
